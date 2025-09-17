321c9ea30eccaaaeed68403cca43a59cb9f7bba8
Joel Kinny <joelkny97@gmail.com>
2025-09-17 14:22:11 -0400

    Time: 12 ms (30.99%) | Memory: 18.3 MB (44.90%) - LeetSync

---
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
+        
+        n = len(grid)
+        a=-1
+        b=-1
+        hashmap = {i:0 for i in range(1,n**2 + 1)}
+        missing = set()
+        maxval = -1
+        minval = 2
+        for i in grid:
+            for j in i:
+                if hashmap.get(j) == 0:
+                    hashmap[j]+=1
+
+                else:
+                    hashmap[j] +=1
+                    a = j
+        
+        for i in hashmap:
+            if hashmap[i]==0:
+                return [a, i]
+
+        return [a,b]
+
+            
+
