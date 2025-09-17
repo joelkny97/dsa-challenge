978b98900f5f7503fc12b30d6ff556471a3014d3
Joel Kinny <joelkny97@gmail.com>
2025-09-17 02:39:10 -0400

    Time: 268 ms (65.92%) | Memory: 17.8 MB (83.24%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -4,30 +4,56 @@ class Solution:
             return ""
 
         n = len(s)
-        max_len = 0
-        start = 0
-        end = 0
+        self.max_len = 0
+        self.start = 0
+        self.end = 0
+        # DP Approach
+        # dp = [[False] * n for _ in range(n)]
 
-        dp = [[False] * n for _ in range(n)]
+        # for i in range(n):
+        #     for j in range(i+1):
+        #         if s[j] == s[i]:
 
-        for i in range(n):
-            for j in range(i+1):
-                if s[j] == s[i]:
+        #             if i-j<= 1 or dp[i-1][j+1]:
+        #                 dp[i][j] = True
+        #                 if max_len < i-j+1:
+        #                     max_len = i-j+1
+        #                     start = j
+        #                     end = i
+        #             else:
+        #                 dp[i][j] = False
 
-                    if i-j<= 1 or dp[i-1][j+1]:
-                        dp[i][j] = True
-                        if max_len < i-j+1:
-                            max_len = i-j+1
-                            start = j
-                            end = i
-                    else:
-                        dp[i][j] = False
-
-                else:
-                    dp[i][j] = False
+        #         else:
+        #             dp[i][j] = False
 
         
-        return s[start:end+1]
+        # return s[start:end+1]
+
+        #Two pointer approach
+
+        for i in range(n):
+            self.extend_around_center(s, i, i) # odd lengths
+            if i<n-1 and s[i] == s[i+1]:
+                self.extend_around_center(s, i, i+1) # even lengths
+
+        return s[self.start:self.end+1]
+
+        
+    def extend_around_center(self, s: str, left: int, right: int):
+        while left>=0 and right<=len(s)-1 and s[left] == s[right]:
+            left-=1
+            right+=1
+
+        left+=1
+        right-=1
+
+        if self.max_len < right-left+1:
+            self.max_len = right-left+1
+            self.start = left
+            self.end = right
+
+        
+
 
 
 
