3d2a9e7008a26b55a88beb6344a3749e1809aa6d
Joel Kinny <joelkny97@gmail.com>
2025-10-06 00:07:56 -0400

    Time: 0 ms (100.00%) | Memory: 16.8 MB (88.62%) - LeetSync

--- 95-unique-binary-search-trees-ii/unique-binary-search-trees-ii.cpp
+++ 95-unique-binary-search-trees-ii/unique-binary-search-trees-ii.cpp
@@ -22,7 +22,7 @@ public:
 private:
 
     vector<TreeNode*> buildTrees(int start, int end) {
-        // memoization
+        // memoization if current range is already computed, return that subtree
         string key = to_string(start)+"-"+to_string(end);   
         if (memo.find(key) != memo.end()) {
             return memo[key];
@@ -35,20 +35,29 @@ private:
             return trees;
         }
 
+        // generate recursively for a given range of values its left and right subtree for a particular rootVal value
         for (int rootVal = start; rootVal<= end; rootVal++) {
+            // In BST root > all nodes in left subtree and root < all nodes in right subtree
+            // right trees are built from start to rootval -1 
+            // left trees are built from rootval + 1 to end
             vector<TreeNode*> leftSubtrees = buildTrees(start, rootVal-1);
             vector<TreeNode*> rightSubtrees = buildTrees(rootVal+1, end);
 
+            // connect all nodes of left and right subtree accordingly
             for (TreeNode* leftTree: leftSubtrees) {
                 for (TreeNode* rightTree: rightSubtrees) {
+                    // create root node
                     TreeNode* root = new TreeNode(rootVal);
+                    // attach its left subtree
                     root->left = leftTree;
+                    // attach its right subtree
                     root->right = rightTree;
+                    // add this root node to the list of trees
                     trees.push_back(root);
                 }
             }
         }
-
+        // store this list of trees for a particular range to be reused
         memo[key] = trees;
         return trees;
         
