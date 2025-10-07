736d5cc203069f7d18dbde11c845d34820fd6091
Joel Kinny <joelkny97@gmail.com>
2025-10-06 22:10:09 -0400

    Time: 10 ms (72.64%) | Memory: 35 MB (85.46%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    int shipWithinDays(vector<int>& weights, int days) {
+
+        int left = 0; // search window starts from max weight (most that the capacity can support)
+
+        int right = 0; // search window ends at the total weight present in weights array
+
+        for(auto w: weights){
+            left = max(w, left);
+            right += w;
+        }
+
+
+        while (left < right ){
+            int mid = left + (right-left) /2;
+
+            if (isFeasible(weights,days, mid) ){
+                // this capacity is capable to ship within days
+                // decrease search window till mid
+                right = mid;
+            }
+            else{
+                // move search window leftward
+                left = mid + 1;
+            }
+        }
+
+        return left;
+        
+
+    }
+
+private:
+    bool isFeasible(const vector<int>& weights, const int days,int capacity){
+
+        int day=1;
+        int total = 0;
+
+        for (auto w: weights){
+            total+=w;
+
+            if (total > capacity){
+                total = w; // reset total to current weight item
+                day+=1; // weight cannot be fit within capacity, need another day
+
+                if (day > days) return false; // cannot ship within days
+            }   
+        }
+
+        return true; // possible to ship within days at this capacity
+
+
+    }
+};
